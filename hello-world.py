32ef112838f2913acbe40606b86c226fc8e32cb1
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-06-04 18:03:27 +0700

    apple orange

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,18 @@
-import sklearn
+from sklearn import tree
+
+# features
+# 0 = smooth
+# 1 = bumpy
+features = [[140, 0], [130, 0], [150, 1], [170, 1]]
+
+# labels
+# 0 = apple
+# 1 = orange
+labels = [0, 0, 1, 1]
+
+clf = tree.DecisionTreeClassifier()
+
+# fit ==> training algorithm to find patterns and data
+clf = clf.fit(features, labels)
+
+print clf.predict([160, 1])
